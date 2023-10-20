85d506297266b2036b337b69183abb6260df6a62
flame123flame <flame.tana@gmail.com>
2023-10-21 00:08:54 +0700

    add

--- lib/service/configDio.dart
+++ lib/service/configDio.dart
@@ -5,8 +5,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Dio getDio() {
   // var uri = "http://192.168.1.187:9000/utcc-mini-project-mobile";
-  // var uri = "http://172.20.10.3:9000/utcc-mini-project-mobile";
-  var uri = "http://192.168.101.132:9000/utcc-mini-project-mobile";
+  var uri = "http://49.0.80.15:2222/utcc-project-api";
+  // var uri = "http://192.168.101.132:9000/utcc-mini-project-mobile";
   FlutterSecureStorage storageToken = new FlutterSecureStorage();
   var _dio = Dio();
   _dio.options.baseUrl = uri;
